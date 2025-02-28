ca4d451aa0ecce19d28f1603e7f7305009df7a87
Shadrack Kiprono <shadrackkiprono00@gmail.com>
2025-02-28 10:16:43 -0800

    arrays

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name); 
+    
+  }
+  
+  
+  function appendCat(name) {
+    return [...cats, name]; 
+    
+  }
+  
+  function prependCat(name) {
+    return [name, ...cats];
+  }
+
+  function destructivelyPrependCat(name) {
+    cats.unshift(name);
+  }
+
+  function destructivelyRemoveLastCat() {
+    cats.pop();
+  }
+  
+  function removeLastCat() {
+    return cats.slice(0, -1);
+  }
+
+  function destructivelyRemoveFirstCat() {
+    cats.shift();
+  }
+  
+  function removeFirstCat() {
+    return cats.slice(1);
+  }
